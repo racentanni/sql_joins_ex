add02b35c74de77c7997cfa5eb34b848fba1637b
racentanni <r.a.centanni@gmail.com>
2024-05-14 14:24:48 -0500

    second commit

--- queries.sql
+++ queries.sql
@@ -1,1 +1,21 @@
 -- write your queries here
+
+--first query
+SELECT * FROM owners o FULL OUTER JOIN vehicles v ON o.id=v.owner_id;
+
+--Second query
+SELECT first_name, last_name,
+COUNT(owner_id) FROM owners o JOIN vehicles v ON o.id=v.owner_id
+GROUP BY (first_name, last_name)
+ORDER BY first_name;
+
+--Third Query
+SELECT
+first_name, last_name,
+ROUND(AVG(price)) AS average_price,
+COUNT(owner_id)
+FROM owners o JOIN vehicles v ON o.id=v.owner_id
+GROUP BY (first_name, last_name)
+HAVING
+COUNT(owner_id) > 1 AND ROUND(AVG(price)) > 10000
+ORDER BY first_name DESC;
